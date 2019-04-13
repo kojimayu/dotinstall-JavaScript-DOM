049aafddf53226acd3a2516d77cc25176318f2c1
kojimayu <bousouko777@gmail.com>
2019-04-13 16:12:22 +0900

    09end

---
+++ .history/js/main_20190413160355.js
@@ -0,0 +1,11 @@
+'use strict'
+
+{
+  const div = document.querySelector('div');
+
+  // div.className = 'box border-pink';
+
+  div.classList.add('border-pink');
+  // div.classList.add('box');
+
+}
---
+++ .history/js/main_20190413160913.js
@@ -0,0 +1,15 @@
+'use strict'
+
+{
+  const div = document.querySelector('div');
+
+  // div.className = 'box border-pink';
+
+  // div.classList.add('border-pink');
+  // div.classList.remove('box');
+  if (div.classList.contains('border-pink')) {
+    div.classList.remove('border-pink');
+  } else {
+    div.classList.add('bordr-pink');
+  }
+}
